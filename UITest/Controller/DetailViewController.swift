9d30ba3a33d41a3e6721bec876050a1ce9cf7166
sh990920 <sh990920@gmail.com>
2024-05-29 10:34:49 +0900

    fix - 주석 추가, button 생성 함수 수정

--- UITest/Controller/DetailViewController.swift
+++ UITest/Controller/DetailViewController.swift
@@ -17,21 +17,22 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // imageView 셋팅
         let image = UIImage(named: info!.name)
         imageView.image = image
-        
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        
+        // imageView 위치 조정
         imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         imageView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
+        // stackView 생성 및 세팅
         let stackView = createInfoStackView()
-        
+        // 화면에 stackView 추가
         view.addSubview(stackView)
-        
+        // stackView 위치 조정
         stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
         stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
         stackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10).isActive = true
@@ -39,13 +40,16 @@ class DetailViewController: UIViewController {
         stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
     }
     
-    
+    // 정보를 나타내는 stackView 생성 함수
     func createInfoStackView() -> UIStackView {
+        // 기본 셋팅
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.distribution = .fillEqually
         stackView.spacing = 20
+        
+        // stackView 안에 들어갈 lable, button 생성
         let nameLable = createLable("name")
         let ageLabel = createLable("age")
         let areaLable = createLable("area")
@@ -65,12 +69,14 @@ class DetailViewController: UIViewController {
         return stackView
     }
     
+    // 버튼을 정렬할 stackView 생성
     func createButtonStackView() -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.distribution = .fillEqually
         stackView.spacing = 20
+        // 버튼 추가
         let gitButton = createButton("git")
         let blogButton = createButton("blog")
         
@@ -80,24 +86,31 @@ class DetailViewController: UIViewController {
         return stackView
     }
     
+    // 버튼 추가
     func createButton(_ platform: String) -> UIButton {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        // 이동할 버튼에 따라 text 변경
         switch(platform) {
         case "git":
-            button.setTitle("git", for: .normal)
+            button.setTitle("깃허브", for: .normal)
+            button.tag = 1
         case "blog":
-            button.setTitle("blog", for: .normal)
+            button.setTitle("블로그", for: .normal)
+            button.tag = 2
         default:
             button.setTitle("nil", for: .normal)
         }
+        // 버튼을 클릭했을 때 작동할 함수 설정
         button.addTarget(self, action: #selector(didTabButton), for: .touchUpInside)
         return button
     }
     
+    // 정보를 저장할 label 을 만드는 함수
     func createLable(_ type: String) -> UILabel {
         let label = UILabel()
+        // label 타입에 따라 다르게 지정
         switch(type) {
         case "name":
             if let name = info?.name {
@@ -131,23 +144,22 @@ class DetailViewController: UIViewController {
         return label
     }
     
-    // 나중에 tag 로 수정 예정
+    // 버튼을 클릭했을 때 실행할 함수
     @objc func didTabButton(_ sender: UIButton) {
-        if let buttonText = sender.titleLabel?.text {
-            switch(buttonText) {
-            case "git":
-                let url = info?.gitHubURL
-                goToPlatform(url!)
-            case "blog":
-                let url = info?.blogURL
-                goToPlatform(url!)
-            default:
-                return
-            }
+        let buttonTag = sender.tag
+        switch(buttonTag) {
+        case 1:
+            let url = info?.gitHubURL
+            goToPlatform(url!)
+        case 2:
+            let url = info?.blogURL
+            goToPlatform(url!)
+        default:
+            return
         }
-        
     }
     
+    // 인터넷 브라우저로 연결하기 위한 함수
     func goToPlatform(_ url: String) {
         let url = URL(string: url)
         let safariViewController = SFSafariViewController(url: url!)
